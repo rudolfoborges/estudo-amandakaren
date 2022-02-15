2feebda37b2fc906cfc1c1d7f5098c48e3309688
Rudolfo Borges <adp_roliveira@uolinc.com>
2022-02-14 22:28:41 -0300

    Desafio 6, implementar um blog

---
+++ javascript/teste6.js
@@ -0,0 +1,6 @@
+//sistema de blog
+//o blob deve permitir cadastrar usuário
+//o usuário do tipo escritor poderá escrever posts no blog
+//os usuários normais poderam comentar nos posts
+//o blog deve exebir todos os posts com o usuário que postou e quantos comentários ele tem
+//dado um post, deve ser possível comentar e visualizar todos os comentários do post
